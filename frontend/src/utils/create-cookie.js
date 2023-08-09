20e174ae6c7b13c806e3953c8ebdf086de26e2c7
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-08-09 14:34:18 +0700

    fix

--- frontend/src/utils/create-cookie.js
+++ frontend/src/utils/create-cookie.js
@@ -1,9 +1,5 @@
 import axios from '../libs/axios'
 
-function hasCsrfCookie() {
-  return document.cookie.split(';').some((item) => item.trim().startsWith('XSRF-TOKEN='))
-}
-
 export default async function createCsrfCookie() {
     const res = await axios.get('/sanctum/csrf-cookie')
     if (res.status === 200) {
