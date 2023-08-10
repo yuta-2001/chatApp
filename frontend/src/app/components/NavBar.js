039d9e6e198a40bb70a06b8586308a9ff3111e64
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-08-10 16:37:24 +0700

    fix

--- frontend/src/app/components/NavBar.js
+++ frontend/src/app/components/NavBar.js
@@ -45,7 +45,7 @@ export default function NavBar() {
     <aside className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
       <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
-            <li>
+            <li className="mb-4">
                 <span href="#" className="flex items-center p-2 text-gray-900 rounded-lg">
                   <img src={user.icon} width={30} height={30} className="w-12 h-12 rounded-full" />
                   <span className="ml-3">{user.name}</span>
