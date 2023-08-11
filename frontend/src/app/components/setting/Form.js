8e91b1222632301280d8ef6548ff6a76427311ef
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-08-11 13:39:54 +0700

    fix

--- frontend/src/app/components/setting/Form.js
+++ frontend/src/app/components/setting/Form.js
@@ -7,8 +7,9 @@ export default function Form() {
     <form>
       <FormImage />
       <FormInput type="email" name="email" id="email" label="Email" required />
-      <FormInput type="password" name="password" id="password" label="password" />
-      <FormInput type="password" name="repeat_password" id="repeat_password" label="Confirm password" />
+      <FormInput type="password" name="current_password" id="current_password" label="Current_password" />
+      <FormInput type="password" name="new_password" id="new_password" label="New password" />
+      <FormInput type="password_confirm" name="password_confirm" id="password_confirm" label="Confirm password" />
       <FormInput type="text" name="name" id="name" label="Name" required />
       <div class="grid md:grid-cols-2 md:gap-6">
         <FormInput type="tel" name="tel" id="tel" label="Tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
