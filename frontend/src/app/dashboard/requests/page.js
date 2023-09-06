6feaf071b0cbdd2813e6c621575dfc7d9cd9b555
yuta-2001 <yu0210ta.sug@gmail.com>
2023-09-06 14:45:56 +0700

    request list and request approve or deny from frontend

--- frontend/src/app/dashboard/requests/page.js
+++ frontend/src/app/dashboard/requests/page.js
@@ -2,9 +2,11 @@
 import { useState, useEffect } from 'react';
 import axios from '../../../libs/axios'
 import { handleErrorResponse } from '../../../utils/handle-error-response'
+import { useToastUpdate } from '../../../contexts/ToastProvider'
 
 export default function RequestPage() {
   const [users, setusers] = useState([]);
+  const setToast = useToastUpdate();
 
   useEffect(() => {
     axios.get('/api/friend-requests/received-list').then((res) => {
@@ -14,9 +16,62 @@ export default function RequestPage() {
     })
   }, [])
 
+  const handleAccept = (id, e) => {
+    e.preventDefault();
+    axios.put('/api/friend-requests', {
+      action: 'accept',
+      requester_id: id
+    }).then(() => {
+      setToast({
+        type: 'success',
+        message: 'success approve request',
+      })
+      axios.get('/api/friend-requests/received-list').then((res) => {
+        setusers(res.data.data);
+      }).catch((err) => {
+        handleErrorResponse(err.response, setToast);
+      })
+    }).catch((err) => {
+      handleErrorResponse(err.response, setToast);
+    })
+  }
+
+  const handleDeny = (id, e) => {
+    e.preventDefault();
+    axios.put('/api/friend-requests', {
+      action: 'deny',
+      requester_id: id
+    }).then(() => {
+      setToast({
+        type: 'warning',
+        message: 'success deny request',
+      })
+      axios.get('/api/friend-requests/received-list').then((res) => {
+        setusers(res.data.data);
+      }).catch((err) => {
+        handleErrorResponse(err.response, setToast);
+      })
+    }).catch((err) => {
+      handleErrorResponse(err.response, setToast);
+    })
+  }
+
   return (
     <div className="p-4">
       <ul className="w-full divide-y divide-gray-200 dark:divide-gray-700">
+        {
+          users.length === 0 && (
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center space-x-4">
+                <div className="flex-1 min-w-0">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                      No request
+                    </p>
+                </div>
+              </div>
+            </li>
+          )
+        }
         {
           users.map((user) => {
             return (
@@ -34,12 +89,12 @@ export default function RequestPage() {
                       </p>
                   </div>
                   <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    <form>
+                    <form onSubmit={(event) => handleAccept(user.id, event)}>
                       <button className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Accept
                       </button>
                     </form>
-                    <form>
+                    <form onSubmit={(event) => handleDeny(user.id, event)}>
                       <button className="inline-flex items-center ml-2 px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                         Reject
                       </button>
